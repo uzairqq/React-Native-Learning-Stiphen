2c75a4c285a9d985a0472904555e47ff6e87ceef
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-14 11:41:25 +0500

    Component Screen

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ComponentScreen = () => {
+  return (
+    <View>
+      <Text>Component Screen</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  textStyle: {
+    fontSize: 15,
+    color: "green",
+  },
+});
+
+export default ComponentScreen;
