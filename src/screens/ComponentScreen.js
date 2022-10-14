f12aea3023c034e2d865d381afe370cdfd228428
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-14 12:49:45 +0500

    Using And Worked with Const Variables in Jsx View

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -2,20 +2,21 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const ComponentScreen = () => {
+  const greet = "Bye There";
+
   return (
     <View>
-      <Text style={{ fontSize: 25, color: "red" }}>
-        This is Component Screen
-      </Text>
+      <Text style={styles.textStyle}>This is Component Screen</Text>
+      <Text>{greet}</Text>
     </View>
   );
 };
 
-// const style = StyleSheet.create({
-//   textStyle: {
-//     fontSize: 30,
-//     color: "green",
-//   },
-// });
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+    color: "green",
+  },
+});
 
 export default ComponentScreen;
