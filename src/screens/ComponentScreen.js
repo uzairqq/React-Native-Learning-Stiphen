a2da34b76a0bdaf2a665aac4c0cb718d6d1a8523
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-14 12:21:58 +0500

    Worked and Used Inline Styling

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -4,16 +4,18 @@ import { View, Text, StyleSheet } from "react-native";
 const ComponentScreen = () => {
   return (
     <View>
-      <Text style={style.textStyle}>This is Component Screen</Text>
+      <Text style={{ fontSize: 25, color: "red" }}>
+        This is Component Screen
+      </Text>
     </View>
   );
 };
 
-const style = StyleSheet.create({
-  textStyle: {
-    fontSize: 50,
-    color: "green",
-  },
-});
+// const style = StyleSheet.create({
+//   textStyle: {
+//     fontSize: 30,
+//     color: "green",
+//   },
+// });
 
 export default ComponentScreen;
