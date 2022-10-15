ee61f19bc83c0dbc784756207eaf525c7d15e2ee
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-15 10:06:17 +0500

    Create List Screen Component and Registered on App.js

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentScreen from "./src/screens/ComponentScreen";
 import PracticeComponentScreen from "./src/screens/PracticeComponentScreen";
+import ListScreenComponent from "./src/screens/ListScreenComponent";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentScreen,
     PracticeComponents: PracticeComponentScreen,
+    ListComponent: ListScreenComponent,
   },
   {
-    initialRouteName: "PracticeComponents",
+    initialRouteName: "ListComponent",
     defaultNavigationOptions: {
       title: "App",
     },
---
+++ src/screens/ListScreenComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListScreenComponent = () => {
+  return (
+    <View>
+      <Text>List Screen Component</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default ListScreenComponent;
