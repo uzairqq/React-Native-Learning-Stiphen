cedee08bfb881db033ea775fcca2e779b5a1496c
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-15 10:36:36 +0500

    Rendering Flatlist In List Screen Component

--- src/screens/ListScreenComponent.js
+++ src/screens/ListScreenComponent.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreenComponent = () => {
+  const freinds = [
+    { id: 1, name: "freind#1" },
+    { id: 2, name: "freind#2" },
+    { id: 3, name: "freind#3" },
+    { id: 4, name: "freind#4" },
+    { id: 5, name: "freind#5" },
+    { id: 6, name: "freind#6" },
+    { id: 7, name: "freind#7" },
+  ];
+
   return (
     <View>
       <Text>List Screen Component</Text>
+      <FlatList
+        data={freinds}
+        renderItem={(element) => {
+          return <Text>{element.item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
