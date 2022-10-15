b641815d9d8cfbbc7fe1fbc7c0ac1c3f8f09e95b
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-15 11:19:33 +0500

    Flatlist Key issue Resolved by Key Extractor

--- src/screens/ListScreenComponent.js
+++ src/screens/ListScreenComponent.js
@@ -3,19 +3,20 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreenComponent = () => {
   const freinds = [
-    { id: 1, name: "freind#1" },
-    { id: 2, name: "freind#2" },
-    { id: 3, name: "freind#3" },
-    { id: 4, name: "freind#4" },
-    { id: 5, name: "freind#5" },
-    { id: 6, name: "freind#6" },
-    { id: 7, name: "freind#7" },
+    { name: "freind#1" },
+    { name: "freind#2" },
+    { name: "freind#3" },
+    { name: "freind#4" },
+    { name: "freind#5" },
+    { name: "freind#6" },
+    { name: "freind#7" },
   ];
 
   return (
     <View>
       <Text>List Screen Component</Text>
       <FlatList
+        keyExtractor={(freind) => freind.name}
         data={freinds}
         renderItem={(element) => {
           return <Text>{element.item.name}</Text>;
