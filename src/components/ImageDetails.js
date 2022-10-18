238d5ad9d7a47028486c3989bec5ed14ff5cb071
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-18 13:54:33 +0500

    Added React Parent Child RelationShip

---
+++ src/components/ImageDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ImageDetails = () => {
+  return (
+    <View>
+      <Text>This is Image Details</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});
+
+export default ImageDetails;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ImageDetails from "../components/ImageDetails";
 
 const ImageScreen = () => {
   return (
     <View>
-      <Text>This is Image Screen</Text>
+      <ImageDetails />
+      <ImageDetails />
+      <ImageDetails />
+      <ImageDetails />
     </View>
   );
 };
