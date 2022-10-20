7b9ff7349d12d80ab3e7f5f0ec8bc7c6f4c92f4b
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-20 19:19:15 +0500

    Generating Randome Colors

--- src/screens/ColoursScreen.js
+++ src/screens/ColoursScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
 const ColoursScreen = () => {
+  const randomColors = () => {
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    return `rgb(${red},${blue},${green})`;
+  };
+
   return (
     <View>
       <Button title="Add a Colour" />
+      <View
+        style={{ height: 100, width: 100, backgroundColor: randomColors() }}
+      />
     </View>
   );
 };
